31e4a8ddc159a229a7992eb361bac8207f601c55
Rachid <rachidboubekeur@free.fr>
2021-04-21 06:24:55 +0200

    refactor(index): amélioration du code html js et conversion des images

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,14 @@
-// Récupère la liste des photographes et la liste des medias dans le fichier data.json
+// Retourne la liste des photographes et la liste des medias dans le fichier data.json
 fetch('data.json')
     .then(response => response.json())
     .then(listDataPhotographers => { listPhotographers(listDataPhotographers['photographers'])})
     .then(listDataMedia => { listMedia(listDataMedia['media'])})
 
+/**
+ * getDataFiltre - Retourne la liste des photographes dans le fichier data.json
+ * @param  {String} filtre
+ */
 function getDataFiltre(filtre) {
-    // Récupère la liste des photographes dans le fichier data.json
     fetch('data.json')
         .then(response => response.json())
         .then(listData => { listDataFiltre(listData['photographers'], filtre)})
--- js/index.js
+++ js/index.js
@@ -7,59 +7,137 @@ window.addEventListener('scroll', () => {
     }
 })
 
+/**
+ * clickNavFiltreTag - renvoi le filtre de navigation demandé a la function getDataFiltre
+ */
+function clickNavFiltreTag() {
+    let navTag = document.querySelector('nav > ul');
+    // Parcours tous les filtres
+    for(let i = 0; i < 8; i++) {
+        let filtreNav = navTag.querySelectorAll('a.tag')[i];
+        filtreNav.addEventListener('click', () => {
+            filtreNav = filtreNav.innerText.toLowerCase().split('#').join('');
+            getDataFiltre(filtreNav);
+        })
+    }
+}
+
+/**
+ * displayListPhotographer - affiche le code html de la liste des photographes
+ * @param  {String} htmlListData code html de la liste des photographes
+ */
+function displayListPhotographer(htmlListData) {
+    document.querySelector('#contenu').innerHTML = htmlListData;
+    // Fermeture du loader et affichage de la liste
+    setTimeout(() => { 
+        document.querySelector('main > p').style.display = "none";
+        document.querySelector('#contenu').style.opacity = 1;
+    }, 1500);
+}
+
+/**
+ * clickArticleFiltreTag - renvoi le filtre du photographe demandé a la function getDataFiltre
+ */
+function clickArticleFiltreTag() {
+    let section = document.querySelector('section');
+    // Parcours la liste des photographes
+    for(let i = 0; i < 6; i++) {
+        if(section.querySelectorAll('article')[i]) {
+            let article = section.querySelectorAll('article')[i];
+            // Parcours les tags du photographe
+            for(let y = 0; y < 4; y++) {
+                if (article.querySelectorAll('a.tag')[y]) {
+                    let filtreArticle = article.querySelectorAll('a.tag')[y];
+                    filtreArticle.addEventListener('click', () => {
+                        filtreArticle = filtreArticle.innerText.split('#').join('');
+                        getDataFiltre(filtreArticle);
+                    })
+                }
+            }
+        }
+    }
+}
+
+/**
+ * listPhotographers - stock la liste des photographes dans htmlListData
+ * @param  {Array} photographers liste des photographes
+ */
 function listPhotographers(photographers) {
  
     let htmlListData = "";
     // Parcours la liste des photographes
     for(let i = 0; i < photographers.length; i++) {
-
         let htmlListTag = "";
         // Parcours les tags du photographe
         for(let y = 0; y < photographers[i].tags.length; y++) {
             // Ajout successif des tags
-            htmlListTag += '<a href=\"#' + photographers[i].tags[y] + '\" onclick=\"getDataFiltre(\'' + photographers[i].tags[y] + '\')\" class=\"tag\" aria-label=\"Filtre portrait\"><span>#' + photographers[i].tags[y] + '</span></a>';
+            htmlListTag += '<a href=\"#' + photographers[i].tags[y] + '\
+                " class=\"tag\" aria-label=\"Filtre portrait\
+                "><span>#' + photographers[i].tags[y] + '</span></a>';
         }
         // Ajout successif des photographes
-        htmlListData += "<article id=\"" + photographers[i].id + "\" aria-label=\"Photographe " + photographers[i].name + "\"><a href=\"photographer.html?id=" + photographers[i].id + "\" aria-label=\"Lien page photographe\"><img src=\"Sample_Photos/Photographers_ID_Photos/" + photographers[i].portrait + "\" alt=\"Portrait de " + photographers[i].name + "\" /><h2>" + photographers[i].name + "</h2></a><p class=\"location\">" + photographers[i].city + ", " + photographers[i].country + "</p><p class=\"tagline\">" + photographers[i].tagline + "</p><p class=\"price\">" + photographers[i].price + "€/jour</p>" + htmlListTag + "</article>";
+        htmlListData += "<article id=\"" + photographers[i].id + 
+            "\" aria-label=\"Photographe " + photographers[i].name
+            + "\"><a href=\"photographer.html?id=" + photographers[i].id
+            + "\" aria-label=\"Lien page photographe\"><img src=\"Sample_Photos/Photographers_ID_Photos/"
+            + photographers[i].portrait + "\" alt=\"Portrait de " + photographers[i].name
+            + "\" /><h2>" + photographers[i].name + "</h2></a><p class=\"location\">"
+            + photographers[i].city + ", " + photographers[i].country + "</p><p class=\"tagline\">"
+            + photographers[i].tagline + "</p><p class=\"price\">" + photographers[i].price
+            + "€/jour</p>" + htmlListTag + "</article>";
     }
 
-    // Incrémentation de la liste dans la section HTML
-    document.querySelector('#contenu').innerHTML = htmlListData;
-
-    // Fermeture du loader et affichage de la liste
-    setTimeout(() => { 
-        document.querySelector('main > p').style.display = "none";
-        document.querySelector('#contenu').style.opacity = 1;
-    }, 2000);
+    clickNavFiltreTag();
+    displayListPhotographer(htmlListData);
+    clickArticleFiltreTag();
 }
 
+/**
+ * listDataFiltre - stock la liste des photographes en fonction du filtre choisi
+ * @param {Array} photographers liste des photographes
+ * @param {String} filtre filtre choisi
+ */
 function listDataFiltre(photographers, filtre) {
 
     document.querySelector("header > p > a").style.display = "none";
     document.querySelector("section").style.justifyContent = "space-evenly";
+    
     let htmlListData = "";
     // Parcours la liste des photographes
     for(let i = 0; i < photographers.length; i++) {
         // Parcours les tags du photographe
         for(let y = 0; y < photographers[i].tags.length; y++) {
-            // Si le tag correspond au filtre demandé
+            // Si le tag correspond au filtre demandé on stock tous les tags du photographe
             if(photographers[i].tags[y] === filtre) {
                 let htmlListTag = "";
                 for(let x = 0; x < photographers[i].tags.length; x++) {
                     if(photographers[i].tags[x] === filtre) {
                         // Ajout du tag égale au filtre
-                        htmlListTag += '<a href=\"#' + photographers[i].tags[x] + '\" onclick=\"getDataFiltre(\'' + photographers[i].tags[x] + '\')\" class=\"tag\" style=\"color:white;background-color:#901C1C\" aria-label=\"Filtre portrait\"><span>#' + photographers[i].tags[x] + '</span></a>';
+                        htmlListTag += '<a href=\"#' + photographers[i].tags[x]
+                        + '\" class=\"tag\" style=\"color:white;background-color:#901C1C\
+                        " aria-label=\"Filtre portrait\"><span>#' + photographers[i].tags[x] + '</span></a>';
                     } else {
                         // Ajout successif des autres tags
-                        htmlListTag += '<a href=\"#' + photographers[i].tags[x] + '\" onclick=\"getDataFiltre(\'' + photographers[i].tags[x] + '\')\" class=\"tag\" aria-label=\"Filtre portrait\"><span>#' + photographers[i].tags[x] + '</span></a>';
+                        htmlListTag += '<a href=\"#' + photographers[i].tags[x]
+                        + '\" class=\"tag\" aria-label=\"Filtre portrait\"><span>#'
+                        + photographers[i].tags[x] + '</span></a>';
                     }
-                }
-                // Ajout successif des photographes
-                htmlListData += "<article id=\"" + photographers[i].id + "\" aria-label=\"Photographe " + photographers[i].name + "\" style=\"margin-left:0%;margin-right:0%;\"><a href=\"photographer.html?id=" + photographers[i].id + "\" aria-label=\"Lien page photographe\"><img src=\"Sample_Photos/Photographers_ID_Photos/" + photographers[i].portrait + "\" alt=\"Portrait de " + photographers[i].name + "\" /><h2>" + photographers[i].name + "</h2></a><p class=\"location\">" + photographers[i].city + ", " + photographers[i].country + "</p><p class=\"tagline\">" + photographers[i].tagline + "</p><p class=\"price\">" + photographers[i].price + "€/jour</p>" + htmlListTag + "</article>";
+                } 
+                // Ajout successif des photographes correspondant au filtre
+                htmlListData += "<article id=\"" + photographers[i].id +
+                    "\" aria-label=\"Photographe "+ photographers[i].name +
+                    "\" style=\"margin-left:0%;margin-right:0%;\"><a href=\"photographer.html?id="
+                    + photographers[i].id + "\" aria-label=\"Lien page photographe\"><img src=\"Sample_Photos/Photographers_ID_Photos/"
+                    + photographers[i].portrait + "\" alt=\"Portrait de " + photographers[i].name + "\" /><h2>"
+                    + photographers[i].name + "</h2></a><p class=\"location\">" + photographers[i].city + ", "
+                    + photographers[i].country + "</p><p class=\"tagline\">" + photographers[i].tagline +
+                    "</p><p class=\"price\">" + photographers[i].price + "€/jour</p>" + htmlListTag + "</article>";
                 break;
             }
         }
     }
-    // Incrémentation de la liste filtré dans la section HTML
-    document.querySelector('#contenu').innerHTML = htmlListData;
+    
+    clickNavFiltreTag();
+    displayListPhotographer(htmlListData);
+    clickArticleFiltreTag();
 }

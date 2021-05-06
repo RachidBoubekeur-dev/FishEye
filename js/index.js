0ba557b8a76f8f0f70f9a103460a92ba4b73eb77
Rachid Boubekeur <50206101+RachidBoubekeur-dev@users.noreply.github.com>
2021-05-06 21:39:12 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 let urlPage = new URLSearchParams(document.location.href);
-let page = decodeURIComponent(urlPage).split('//')[1].split('/')[1].split('/')[1].split('.html')[0];
+let page = decodeURIComponent(urlPage).split('//')[1].split('/')[1].split('.html')[0];
 alert(page);
 // Retourne la liste des photographes et la liste des medias dans le fichier data.json
 fetch('data.json')

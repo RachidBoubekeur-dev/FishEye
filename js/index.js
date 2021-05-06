d7ee7ac36683ca36eda153cc15f4814d2254c2bc
Rachid Boubekeur <50206101+RachidBoubekeur-dev@users.noreply.github.com>
2021-05-06 22:09:53 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 let urlPage = new URLSearchParams(document.location.href);
-alert(decodeURIComponent(urlPage));
-let page = decodeURIComponent(urlPage).split('https://')[1];
+let page = decodeURIComponent(urlPage).split('https://rachidboubekeur-dev.github.io/RachidBoubekeur_DA6_12042021/')[1];
 alert(page);
 // Retourne la liste des photographes et la liste des medias dans le fichier data.json
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        if (page === "index") initPhotographer(data['photographers'])
+        if (page === "index.html") initPhotographer(data['photographers'])
         else initDataPage(data['photographers'], data['media'])
     })

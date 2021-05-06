89f89ad6a0331694a34a09fc2c6a96992610cba5
Rachid <rachidboubekeur@free.fr>
2021-05-06 19:16:33 +0200

    refactor(index.js): adaptation control url pour GitHub

--- js/index.js
+++ js/index.js
@@ -1,9 +1,8 @@
 let urlPage = new URLSearchParams(document.location.href);
-let page = decodeURIComponent(urlPage).split('//')[1].split('/')[1].split('.')[0];
 // Retourne la liste des photographes et la liste des medias dans le fichier data.json
 fetch('data.json')
     .then(response => response.json())
     .then(data => {
-        if (page === "index") initPhotographer(data['photographers'])
+        if (decodeURIComponent(urlPage).split('//')[1].split('/')[1].split('.')[0] === "index" || decodeURIComponent(urlPage).split('//')[1].split('/')[1].split('/')[1].split('.')[0] === "index") initPhotographer(data['photographers'])
         else initDataPage(data['photographers'], data['media'])
     })
